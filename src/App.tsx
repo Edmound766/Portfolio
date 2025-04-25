264acd88f0cf9d3f85c6cae83f79d6c136a3493a
Edmound Njoori <edmound776@gmail.com>
2025-04-25 11:10:06 +0300

    This adds the project template

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Layout from './layout'
+import Home from '@pages/Home'
+import About from '@pages/About'
+import Projects from '@pages/Projects'
+import Contact from '@pages/Contact'
+
+
+function App() {
+  return (
+   <>
+   <Router>
+    <Routes >
+      <Route path='/' element={<Layout/>}> 
+        <Route index element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/projects" element={<Projects/>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Route>
+
+    </Routes>
+   </Router>
+   </>)
+}
+
+export default App
---
+++ src/layout.tsx
@@ -0,0 +1,16 @@
+import Navbar from '@components/navigation/navbar'
+import { Outlet } from 'react-router-dom'
+
+export default function Layout() {
+  return (
+    <>
+    <header>
+      <Navbar/>
+    </header>
+    <main>
+        <Outlet/>
+    </main>
+    <footer></footer>
+    </>
+  )
+}
